089c827efce35d1cd12abebde481a3d43603d805
Bjarke Erik Hjalmar Kusk Arvedlund <bja001@edu.zealand.dk>
2025-03-18 13:29:15 +0100

    Andre medlemmer side

---
+++ Gasolin/Pages/AndreMedlemmer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gasolin.Pages
+{
+    public class AndreMedlemmerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
